39117e53d0989ae030a555d74c295fa0015f3237
Vasilis <vasilisgoutz08@gmail.com>
2021-03-21 01:25:26 +0200

    Created and styled search bar.

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -7,15 +7,22 @@ class SearchBar extends React.Component {
         this.state = {
             'city' : ''
         }
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event){
+        this.setState({
+            'city' : event.target.value
+        });
     }
 
     render(){
         return (
             <div className="element-container search-container">
-                <div className="search-form city-form">
-                    <input type="text" className="search-field city-field" placeholder="Type city..."/>
-                    <button type="submit" name="Search" value="Search" className="search-button city">Search</button>
-                </div>
+                <form className="search-form city-form">
+                    <input type="text" className="search-field city-field" placeholder="Type city..." onChange={this.handleChange}/>
+                    <button type="submit" name="Search" className="search-button city"></button>
+                </form>
             </div>
         );
     }
