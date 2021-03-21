cc710914a4f0a8f4aa35facc116ca8229c9d5536
Vasilis <vasilisgoutz08@gmail.com>
2021-03-21 20:39:48 +0200

    Looking for a good weather api.

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -2,12 +2,16 @@ import React from 'react'
 
 class SearchBar extends React.Component {
 
+    // https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=60.10&lon=9.58
+    META_WEATHER_URL = "https://www.metaweather.com/api/location/search/?query=";
+
     constructor(props){
         super(props);
         this.state = {
             'city' : ''
         }
         this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
     }
 
     handleChange(event){
@@ -16,12 +20,34 @@ class SearchBar extends React.Component {
         });
     }
 
+    handleClick(event){
+        if(this.state.city.length >= 3){
+            try {
+                fetch(this.META_WEATHER_URL + this.state.city)
+                .then(response => {
+                    if(response.length !== 0){
+                        response = response.json();
+                        console.log(response);
+                    }
+                    
+                })
+                .then(data => {
+                    console.log(data);
+                });
+            }
+            catch(err){
+                console.log(err);
+            }
+        }
+        event.preventDefault();
+    }
+
     render(){
         return (
             <div className="element-container search-container">
                 <form className="search-form city-form">
                     <input type="text" className="search-field city-field" placeholder="Type city..." onChange={this.handleChange}/>
-                    <button type="submit" name="Search" className="search-button city"></button>
+                    <button type="submit" name="search" className="search-button city" onClick={this.handleClick}></button>
                 </form>
             </div>
         );
