225f3741a3bc04a8fadb166a1232f8d4e1fe7698
Vasilis <goutz@softweb.gr>
2022-04-27 00:29:06 +0300

    Removed demo because Meta Weather's CORS policy blocks the app from working correctly.

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
 A weather app (like there aren't any of them) made in React, using the [Meta Weather API](https://www.metaweather.com/api/) to fetch data.
-
-## Demo
-
-You can view the demo [here](https://vasilisg.github.io/React-Weather-App/).
--- src/components/Constants.js
+++ src/components/Constants.js
@@ -1,6 +1,7 @@
-export const  META_WEATHER_ENDPOINT = "https://meta-weather.now.sh/api/location/search/?query=";
+export const META_WEATHER_ENDPOINT = "https://meta-weather.now.sh/api/location/search/?query=";
 export const META_WEATHER_LOCATION_ENDPOINT = "https://meta-weather.now.sh/api/location/";
 export const WEATHER_IMAGE_ENDPOINT = "https://meta-weather.now.sh/static/img/weather/";
+
 export const DAY_LOOKUP = {
     0 : 'Sunday', 
     1 : 'Monday', 
